90f6e21c5d0b488019ba58643a121553a260ecf2
lihuayong <lxy1148545418@163.com>
2022-06-25 15:47:40 +0800

    商品导入1

---
+++ web/helpers/excel.ts
@@ -0,0 +1,201 @@
+import {resolve, dirname} from "path"
+import {Cell, Row} from 'exceljs';
+import exceljs from 'exceljs';
+const {Workbook} = exceljs;
+const __dirname = resolve()
+
+export type Products = { [key: string]: ProductInfo }
+
+export interface ProductInfo {
+    handle: string;
+    title: string;
+    body_html: string;
+    images: string[] | null;
+    prices: number[] | null;
+    option_names: string[] | null;
+    option_values: string[] | null;
+    // 每行商品的有多少个option， 这样便于区分option_names，option_values 在每行有多少个， 从而避免解析错误
+    option_cnt: number[];
+}
+
+function initProductInfo(): ProductInfo {
+    return {
+        title: "",
+        handle: "",
+        body_html: "",
+        images: null,
+        prices: null,
+        option_names: null,
+        option_values: null,
+        option_cnt: [],
+    };
+}
+
+function handleProductCell(prod: ProductInfo, prop: string, value: any) {
+    // @ts-ignore
+    let p: Array<[any]> = prod[prop];
+    if (p) {
+        p.push(value)
+    } else {
+        // @ts-ignore
+        prod[prop] = [value]
+    }
+}
+
+// 把商品信息的解析放在一个类里，封装起来。
+// 通过调用public方法即可完成商品信息的解析
+export class ParseProductInfo {
+    // 待解析商品表单的索引号
+    private static readonly PRODUCT_SHEET_INDEX: number = 0;
+    private static readonly HANDLE: string = "Handle";
+    private static readonly TITLE: string = "Title";
+    private static readonly BODY_HTML: string = "Body (HTML)";
+    private static readonly IMAGE_SRC: string = "Image Src";
+    private static readonly VARIANT_PRICE: string = "Variant Price";
+    private static readonly OPTION1_NAME: string = "Option1 Name";
+    private static readonly OPTION1_VALUE: string = "Option1 Value";
+    private static readonly OPTION2_NAME: string = "Option2 Name";
+    private static readonly OPTION2_VALUE: string = "Option2 Value";
+    private static readonly OPTION3_NAME: string = "Option3 Name";
+    private static readonly OPTION3_VALUE: string = "Option3 Value";
+    // 待解析的表头数据
+    private static readonly HEADERS: string[] = [
+        ParseProductInfo.HANDLE,
+        ParseProductInfo.TITLE,
+        ParseProductInfo.BODY_HTML,
+        ParseProductInfo.IMAGE_SRC,
+        ParseProductInfo.VARIANT_PRICE,
+        ParseProductInfo.OPTION1_NAME,
+        ParseProductInfo.OPTION1_VALUE,
+        ParseProductInfo.OPTION2_NAME,
+        ParseProductInfo.OPTION2_VALUE,
+        ParseProductInfo.OPTION3_NAME,
+        ParseProductInfo.OPTION3_VALUE,
+
+    ];
+    private fileName: string;
+    // 存储解析的结果
+    private products: Products;
+    // 待解析商品表单的表头行所在索引， 可以避免前面几行不是表头行的情况
+    private headerRowIndex: number;
+    // 表头对应的列
+    private headerCols: { [key: string]: string };
+    // handle 所在的列的位置
+    private headerHandleColIndex: string;
+
+    constructor(fileName: string) {
+        this.fileName = fileName;
+        this.products = {};
+        this.headerCols = {};
+        this.headerRowIndex = -1;
+    }
+
+
+    // 解析excel表格里的商品信息
+    public async parse(): Promise<Products> {
+        if (this.fileName) {
+            const workbook = new Workbook();
+            const p = resolve(__dirname, this.fileName);
+            const worksheets = await workbook.xlsx.readFile(p);
+            if (worksheets.worksheets.length > 0) {
+                const worksheet = worksheets.worksheets[ParseProductInfo.PRODUCT_SHEET_INDEX];
+                // {includeEmpty: false} 不处理空行
+                // 解析单独的商品信息
+                worksheet.eachRow({includeEmpty: false}, ((row, rowNumber) => {
+                    // 首先解析表头行
+                    if (this.headerRowIndex == -1) {
+                        this.parseHeaderRow(row, rowNumber);
+                        // console.log(this.headerCols)
+                    } else {
+                        // 然后再解析数据行
+                        this.parseDataRow(row, rowNumber);
+                    }
+                }))
+            }
+        }
+        return this.products;
+    }
+
+    private static isValidHeader(txt:string): boolean {
+        // 判断是否是表头
+        return ParseProductInfo.HEADERS.indexOf(txt) != -1;
+    }
+
+    // 解析表头行
+    private parseHeaderRow(row: Row, rowNumber: number) {
+        let headerCnt: number = 0;
+        row.eachCell({includeEmpty: false}, (cell: Cell, colNumber: number) => {
+            const txt = cell.text?.trim();
+            if (txt && ParseProductInfo.isValidHeader(txt)) {
+                // 保存表头所在的列
+                this.headerCols[cell.col] = txt;
+                ++headerCnt;
+                if (txt == ParseProductInfo.HANDLE) this.headerHandleColIndex = cell.col;
+            }
+        })
+        // 如果某行里， 都有这些表头， 那就认为改行是表头行
+        if (headerCnt == ParseProductInfo.HEADERS.length) this.headerRowIndex = rowNumber;
+        else if (headerCnt > 0) {
+            // 否则只解析到部分表头， 那就需要重新初始化已经解析过的表头数据。
+            this.headerCols = {};
+        }
+    }
+
+    // 解析数据行
+    private parseDataRow(row: Row, rowNumber: number) {
+        if (this.headerRowIndex != -1 && this.headerHandleColIndex) {
+            const handle: Cell = row.getCell(this.headerHandleColIndex)
+            if (!handle) return;
+            const uniqueHandleTxt = handle.text?.trim();
+            if (!uniqueHandleTxt) return;
+            let prod: ProductInfo = this.products[uniqueHandleTxt];
+            if (!prod) {
+                prod = initProductInfo();
+                prod.handle = uniqueHandleTxt;
+                this.products[uniqueHandleTxt] = prod;
+            }
+            const preOptionCnt = prod.option_values?.length || 0
+            // 处理其他的商品属性
+            row.eachCell({includeEmpty: false}, (cell: Cell, colNumber: number) => {
+                    const headerName = this.headerCols[cell.col]
+                    const txt = cell.text?.trim();
+                    if (txt) {
+                        switch (headerName) {
+                            case ParseProductInfo.TITLE:
+                                prod.title = txt;
+                                break;
+                            case ParseProductInfo.BODY_HTML:
+                                prod.body_html = txt
+                                break;
+                            case ParseProductInfo.IMAGE_SRC:
+                                handleProductCell(prod, "images", txt);
+                                break;
+                            case ParseProductInfo.VARIANT_PRICE:
+                                handleProductCell(prod, "prices", parseFloat(txt));
+                                break;
+                            case ParseProductInfo.OPTION1_NAME:
+                            case ParseProductInfo.OPTION2_NAME:
+                            case ParseProductInfo.OPTION3_NAME:
+                                handleProductCell(prod, "option_names", txt);
+                                break;
+                            case ParseProductInfo.OPTION1_VALUE:
+                            case ParseProductInfo.OPTION2_VALUE:
+                            case ParseProductInfo.OPTION3_VALUE:
+                                handleProductCell(prod, "option_values", txt);
+                                break;
+                            default:
+                                break
+                        }
+                    }
+                }
+            );
+            const curOpntionCnt = prod.option_values?.length || 0;
+            // 只能取 option_values ， 因为 name 可能为空， name为空时， 取上一个
+            const diff = curOpntionCnt - preOptionCnt;
+            if (diff > 0)
+                prod.option_cnt.push(diff)
+        }
+
+    }
+
+}
--- web/index.js
+++ web/index.js
@@ -1,14 +1,15 @@
 // @ts-check
-import { join } from "path";
+import {join} from "path";
 import fs from "fs";
 import express from "express";
 import cookieParser from "cookie-parser";
-import { Shopify, ApiVersion } from "@shopify/shopify-api";
+import {Shopify, ApiVersion} from "@shopify/shopify-api";
 
 import applyAuthMiddleware from "./middleware/auth.js";
 import verifyRequest from "./middleware/verify-request.js";
-import { setupGDPRWebHooks } from "./gdpr.js";
-import { BillingInterval } from "./helpers/ensure-billing.js";
+import {setupGDPRWebHooks} from "./gdpr.js";
+import {BillingInterval} from "./helpers/ensure-billing.js";
+import {ParseProductInfo} from "./helpers/excel.js"
 
 const USE_ONLINE_TOKENS = true;
 const TOP_LEVEL_OAUTH_COOKIE = "shopify_top_level_oauth";
@@ -19,7 +20,7 @@ const isTest = process.env.NODE_ENV === "test" || !!process.env.VITE_TEST_BUILD;
 const versionFilePath = "./version.txt";
 let templateVersion = "unknown";
 if (fs.existsSync(versionFilePath)) {
-  templateVersion = fs.readFileSync(versionFilePath, "utf8").trim();
+    templateVersion = fs.readFileSync(versionFilePath, "utf8").trim();
 }
 
 // TODO: There should be provided by env vars
@@ -29,36 +30,36 @@ const PROD_INDEX_PATH = `${process.cwd()}/frontend/dist/`;
 const DB_PATH = `${process.cwd()}/database.sqlite`;
 
 Shopify.Context.initialize({
-  API_KEY: process.env.SHOPIFY_API_KEY,
-  API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
-  SCOPES: process.env.SCOPES.split(","),
-  HOST_NAME: process.env.HOST.replace(/https?:\/\//, ""),
-  HOST_SCHEME: process.env.HOST.split("://")[0],
-  API_VERSION: ApiVersion.April22,
-  IS_EMBEDDED_APP: true,
-  // This should be replaced with your preferred storage strategy
-  SESSION_STORAGE: new Shopify.Session.SQLiteSessionStorage(DB_PATH),
-  USER_AGENT_PREFIX: `Node App Template/${templateVersion}`,
+    API_KEY: process.env.SHOPIFY_API_KEY,
+    API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
+    SCOPES: process.env.SCOPES.split(","),
+    HOST_NAME: process.env.HOST.replace(/https?:\/\//, ""),
+    HOST_SCHEME: process.env.HOST.split("://")[0],
+    API_VERSION: ApiVersion.April22,
+    IS_EMBEDDED_APP: true,
+    // This should be replaced with your preferred storage strategy
+    SESSION_STORAGE: new Shopify.Session.SQLiteSessionStorage(DB_PATH),
+    USER_AGENT_PREFIX: `Node App Template/${templateVersion}`,
 });
 
 // Storing the currently active shops in memory will force them to re-login when your server restarts. You should
 // persist this object in your app.
 const ACTIVE_SHOPIFY_SHOPS = {};
 Shopify.Webhooks.Registry.addHandler("APP_UNINSTALLED", {
-  path: "/api/webhooks",
-  webhookHandler: async (topic, shop, body) =>
-    delete ACTIVE_SHOPIFY_SHOPS[shop],
+    path: "/api/webhooks",
+    webhookHandler: async (topic, shop, body) =>
+        delete ACTIVE_SHOPIFY_SHOPS[shop],
 });
 
 // The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
 // See the ensureBilling helper to learn more about billing in this template.
 const BILLING_SETTINGS = {
-  required: false,
-  // This is an example configuration that would do a one-time charge for $5 (only USD is currently supported)
-  // chargeName: "My Shopify One-Time Charge",
-  // amount: 5.0,
-  // currencyCode: "USD",
-  // interval: BillingInterval.OneTime,
+    required: false,
+    // This is an example configuration that would do a one-time charge for $5 (only USD is currently supported)
+    // chargeName: "My Shopify One-Time Charge",
+    // amount: 5.0,
+    // currencyCode: "USD",
+    // interval: BillingInterval.OneTime,
 };
 
 // This sets up the mandatory GDPR webhooks. You’ll need to fill in the endpoint
@@ -71,110 +72,125 @@ setupGDPRWebHooks("/api/webhooks");
 
 // export for test use only
 export async function createServer(
-  root = process.cwd(),
-  isProd = process.env.NODE_ENV === "production",
-  billingSettings = BILLING_SETTINGS
+    root = process.cwd(),
+    isProd = process.env.NODE_ENV === "production",
+    billingSettings = BILLING_SETTINGS
 ) {
-  const app = express();
-  app.set("top-level-oauth-cookie", TOP_LEVEL_OAUTH_COOKIE);
-  app.set("active-shopify-shops", ACTIVE_SHOPIFY_SHOPS);
-  app.set("use-online-tokens", USE_ONLINE_TOKENS);
+    const app = express();
+    app.set("top-level-oauth-cookie", TOP_LEVEL_OAUTH_COOKIE);
+    app.set("active-shopify-shops", ACTIVE_SHOPIFY_SHOPS);
+    app.set("use-online-tokens", USE_ONLINE_TOKENS);
 
-  app.use(cookieParser(Shopify.Context.API_SECRET_KEY));
+    app.use(cookieParser(Shopify.Context.API_SECRET_KEY));
 
-  applyAuthMiddleware(app, {
-    billing: billingSettings,
-  });
+    applyAuthMiddleware(app, {
+        billing: billingSettings,
+    });
 
-  app.post("/api/webhooks", async (req, res) => {
-    try {
-      await Shopify.Webhooks.Registry.process(req, res);
-      console.log(`Webhook processed, returned status code 200`);
-    } catch (error) {
-      console.log(`Failed to process webhook: ${error}`);
-      if (!res.headersSent) {
-        res.status(500).send(error.message);
-      }
-    }
-  });
+    app.post("/api/webhooks", async (req, res) => {
+        try {
+            await Shopify.Webhooks.Registry.process(req, res);
+            console.log(`Webhook processed, returned status code 200`);
+        } catch (error) {
+            console.log(`Failed to process webhook: ${error}`);
+            if (!res.headersSent) {
+                res.status(500).send(error.message);
+            }
+        }
+    });
 
-  // All endpoints after this point will require an active session
-  app.use(
-    "/api/*",
-    verifyRequest(app, {
-      billing: billingSettings,
-    })
-  );
-  // https://811a-111-121-95-4.jp.ngrok.io/api/products-count?shop=lee666tech2.myshopify.com
-  app.get("/api/products-count", async (req, res) => {
-    const session = await Shopify.Utils.loadCurrentSession(req, res, true);
-    const { Product } = await import(
-      `@shopify/shopify-api/dist/rest-resources/${Shopify.Context.API_VERSION}/index.js`
+    // All endpoints after this point will require an active session
+    app.use(
+        "/api/*",
+        verifyRequest(app, {
+            billing: billingSettings,
+        })
     );
+    // https://dbaa-111-121-95-4.ap.ngrok.io/api/products-count?shop=lee666tech2.myshopify.com
+    // https://dbaa-111-121-95-4.ap.ngrok.io/api/import/product?shop=lee666tech2.myshopify.com
+    app.get("/api/products-count", async (req, res) => {
+        const session = await Shopify.Utils.loadCurrentSession(req, res, true);
+        const {Product} = await import(
+            `@shopify/shopify-api/dist/rest-resources/${Shopify.Context.API_VERSION}/index.js`
+            );
 
-    const countData = await Product.count({ session });
-    res.status(200).send(countData);
-  });
+        const countData = await Product.count({session});
+        res.status(200).send(countData);
+    });
 
-  app.post("/api/graphql", async (req, res) => {
-    try {
-      const response = await Shopify.Utils.graphqlProxy(req, res);
-      res.status(200).send(response.body);
-    } catch (error) {
-      res.status(500).send(error.message);
+    app.post("/api/graphql", async (req, res) => {
+        try {
+            const response = await Shopify.Utils.graphqlProxy(req, res);
+            res.status(200).send(response.body);
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    });
+    const filePath = "asserts/jewelery.xlsx";
+    app.get("/api/import/product", async (req, res) => {
+        try {
+            console.log("ParseProductInfo1")
+            const pi = new ParseProductInfo(filePath)
+            pi.parse().then(result => {
+                res.status(200).send(result);
+            }).catch(err => {
+                console.error(err)
+                res.status(500).send(err);
+            })
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    });
+    app.use(express.json());
+
+    app.use((req, res, next) => {
+        const shop = req.query.shop;
+        if (Shopify.Context.IS_EMBEDDED_APP && shop) {
+            res.setHeader(
+                "Content-Security-Policy",
+                `frame-ancestors https://${shop} https://admin.shopify.com;`
+            );
+        } else {
+            res.setHeader("Content-Security-Policy", `frame-ancestors 'none';`);
+        }
+        next();
+    });
+
+    if (isProd) {
+        const compression = await import("compression").then(
+            ({default: fn}) => fn
+        );
+        const serveStatic = await import("serve-static").then(
+            ({default: fn}) => fn
+        );
+        app.use(compression());
+        app.use(serveStatic(PROD_INDEX_PATH));
     }
-  });
 
-  app.use(express.json());
+    app.use("/*", async (req, res, next) => {
+        const shop = req.query.shop;
 
-  app.use((req, res, next) => {
-    const shop = req.query.shop;
-    if (Shopify.Context.IS_EMBEDDED_APP && shop) {
-      res.setHeader(
-        "Content-Security-Policy",
-        `frame-ancestors https://${shop} https://admin.shopify.com;`
-      );
-    } else {
-      res.setHeader("Content-Security-Policy", `frame-ancestors 'none';`);
-    }
-    next();
-  });
+        // Detect whether we need to reinstall the app, any request from Shopify will
+        // include a shop in the query parameters.
+        if (app.get("active-shopify-shops")[shop] === undefined && shop) {
+            res.redirect(`/api/auth?shop=${shop}`);
+        } else {
+            // res.set('X-Shopify-App-Nothing-To-See-Here', '1');
+            const fs = await import("fs");
+            const fallbackFile = join(
+                isProd ? PROD_INDEX_PATH : DEV_INDEX_PATH,
+                "index.html"
+            );
+            res
+                .status(200)
+                .set("Content-Type", "text/html")
+                .send(fs.readFileSync(fallbackFile));
+        }
+    });
 
-  if (isProd) {
-    const compression = await import("compression").then(
-      ({ default: fn }) => fn
-    );
-    const serveStatic = await import("serve-static").then(
-      ({ default: fn }) => fn
-    );
-    app.use(compression());
-    app.use(serveStatic(PROD_INDEX_PATH));
-  }
-
-  app.use("/*", async (req, res, next) => {
-    const shop = req.query.shop;
-
-    // Detect whether we need to reinstall the app, any request from Shopify will
-    // include a shop in the query parameters.
-    if (app.get("active-shopify-shops")[shop] === undefined && shop) {
-      res.redirect(`/api/auth?shop=${shop}`);
-    } else {
-      // res.set('X-Shopify-App-Nothing-To-See-Here', '1');
-      const fs = await import("fs");
-      const fallbackFile = join(
-        isProd ? PROD_INDEX_PATH : DEV_INDEX_PATH,
-        "index.html"
-      );
-      res
-        .status(200)
-        .set("Content-Type", "text/html")
-        .send(fs.readFileSync(fallbackFile));
-    }
-  });
-
-  return { app };
+    return {app};
 }
 
 if (!isTest) {
-  createServer().then(({ app }) => app.listen(PORT));
+    createServer().then(({app}) => app.listen(PORT));
 }
